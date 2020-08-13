63677bf655451117cea5fd38a1e303ed6091f951
Wade Moses <wmoses@ewu.edu>
2020-08-12 20:57:44 -0700

    Created a new Hero Healer.java

---
+++ Healer.java
@@ -0,0 +1,56 @@
+public class Healer extends Hero {
+
+    public Healer(final String name) {
+        super(name, Details.getHealerDetails());
+    }
+
+    public void heal() {
+        int hp;
+        int[] healRange = super.getDetails().getHealDetails();
+
+        hp = (int) (Math.random() * (healRange[1] - healRange[0] + 1)) + healRange[0];
+        super.addHitPoints(hp);
+        System.out.println(super.getName() + " added [" + hp + "] points.\n" + "Total hit points remaining are: " + super.getDetails().getHitPoints());
+        System.out.println();
+    }
+
+    public void attack(DungeonCharacter opponent) {
+        if (opponent == null)
+            throw new IllegalArgumentException("Opponent passed as null...");
+
+        System.out.println(super.getName() + " brings a mace down to smite " + opponent.getName() + ":");
+        super.attack(opponent);
+    }
+
+    public void battleChoices(DungeonCharacter opponent) {
+        if (opponent == null)
+            throw new IllegalArgumentException("Opponent passed as null...");
+
+        super.battleChoices(opponent);
+        int choice;
+
+        do {
+            System.out.println("1) Attack Opponent");
+            System.out.println("2) Cast Heal");
+            System.out.print("Choose an option: ");
+            choice = Keyboard.readInt();
+            System.out.println("--------------------------------------------------\n");
+
+            switch (choice)
+		    {
+			    case 1: this.attack(opponent);
+			        break;
+			    case 2: this.heal();
+			        break;
+			    default:
+			        System.out.println("invalid choice!");
+		    }//end switch
+
+
+            super.setNumTurns(super.getNumTurns() - 1);
+            if (super.getNumTurns() > 0)
+                System.out.println("Number of turns remaining is: " + super.getNumTurns());
+        } while (super.getNumTurns() > 0);
+
+    }
+}
